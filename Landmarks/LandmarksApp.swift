4d0be148570a3795e65a43b83b1fd8ae193c0c81
jacklynchiaentsai <jacklyntsai1213@gmail.com>
2023-07-09 15:30:09 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jacklyn Tsai on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
