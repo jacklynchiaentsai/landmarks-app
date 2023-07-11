8e2da94c5e548fc830d0e925be47a2ef853441c7
jacklynchiaentsai <jacklyntsai1213@gmail.com>
2023-07-11 15:27:11 +0800

    update preview settings for landmarks list

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,6 +9,7 @@
  1) passing along with data a key path to a property that uniquely identifies each element
  2) conform data type to Identifiable protocol
  - set navigation capacbilities to a list by embedding it in a NavifationView, and then nesting each row in a NavigationLink
+ - when the elements of array are simple value types use \.self as key path to the identifier
  */
 
 import SwiftUI
@@ -34,6 +35,12 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone SE (3rd generation)", "iPhone XS Max","iPad Pro (12.9-inch) (6th generation)"], id: \.self) { deviceName in
+            LandmarkList()
+            // changing preview to render at different size
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                // add device name as labels for the previews
+                .previewDisplayName(deviceName)
+        }
     }
 }
